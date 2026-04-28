17899a1afc815e9d8d8ef988a3b3dcdc5b3f6a1b
Sovyshkin <zankov.vadik@yandex.ru>
2026-04-28 21:07:16 +0500

    fix bug

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -1,8 +1,19 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 export default defineConfig({
   plugins: [vue()],
+  base: '/admin/',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets'
+  },
   server: {
     port: 5173,
     proxy: {
