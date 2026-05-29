e71d10b19e3aaa4c65cfaef164dc3bab234845ed
Sovyshkin <zankov.vadik@yandex.ru>
2026-05-29 14:30:28 +0500

    some changes

--- vue-app/src/views/Cart.vue
+++ vue-app/src/views/Cart.vue
@@ -563,7 +563,7 @@
               </ul>
             </div>
 
-            <button class="btn btn-primary btn-submit" @click="placeOrder" :disabled="!isFormValid || ordering">
+            <button class="btn btn-primary btn-submit" @click="placeOrder" :disabled="ordering">
               <svg v-if="!ordering" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
                 <path d="M22 11.08V12a10 10 0 11-5.93-9.14"/>
                 <polyline points="22 4 12 14.01 9 11.01"/>
