284c864edfe7a84de5bfc244936bc085a874fd5e
Sovyshkin <zankov.vadik@yandex.ru>
2026-07-30 02:21:18 +0500

    some changes

--- vue-app/src/App.vue
+++ vue-app/src/App.vue
@@ -202,6 +202,7 @@
               <li><router-link to="/guarantees">Гарантии</router-link></li>
               <li><router-link to="/faq">Частые вопросы</router-link></li>
               <li><router-link to="/contact">Контакты</router-link></li>
+              <li><a href="/certificate-pts-105445.pdf" target="_blank" rel="noopener">Сертификат</a></li>
               <li><a href="/policy.pdf" target="_blank" rel="noopener">Политика Конфиденциальности</a></li>
               <li><a href="/public-offer-2026.pdf" target="_blank" rel="noopener">Публичная оферта</a></li>
             </ul>
